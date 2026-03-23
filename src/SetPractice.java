16d5e31d4b2a9dafda76a48c73f7d8beec23da8a
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-03-22 17:36:00 -0700

    completed set practice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,30 @@
+import java.util.Set;
+import java.util.HashSet;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    Set<String> fruits = new HashSet<> ();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    fruits.add("blackberry");
+    fruits.add("raspberry");
+    fruits.add("blueberry");
 
     // Check whether the Set contains a given String
+    System.out.println(fruits.contains("blackberry"));
 
     // Remove an element from the Set
+    fruits.remove("raspberry");
 
     // Get the size of the Set
+    System.out.println(fruits.size());
 
     // Iterate over the elements of the Set, printing each one on a separate line
+    for(String fruit : fruits){
+      System.out.println(fruit);
+    }
 
     /*
      * Warning!
