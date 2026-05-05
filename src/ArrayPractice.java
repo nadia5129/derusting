f172e77ecadcd1d9801807259ace606cedf92e30
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-05-05 14:20:31 -0500

    first commit

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,13 +1,22 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
+    String [] myArray = new String[4];
+    
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
+    myArray[0]= "String one";
+    myArray[1]= "String two";
+    myArray[2]= "string three";
+    myArray[3]= "string four";
+
 
     // Get the value of the array at index 2
+   
 
     // Get the length of the array
+  
 
     // Iterate over the array using a traditional for loop and print out each item
 
