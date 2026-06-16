fe73d0c22a640065dbb5435d96ddb6885f17f758
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-06-16 14:11:24 -0700

    completed number practice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -18,20 +18,39 @@ public class MapPractice {
     javaMap.put("apple", 3);
 
     // Get the value associated with a given key in the Map
+     System.out.println("Value for kiwi: " + javaMap.get("kiwi"));
 
     // Find the size (number of key/value pairs) of the Map
+    System.out.println("Size: " + javaMap.size());
 
     // Replace the value associated with a given key (the size of the Map shoukld not change)
+    System.out.println("contains key" + javaMap.containsKey("apple"));
+  
 
     // Check whether the Map contains a given key
+     System.out.println("contains value 3" + javaMap.containsValue(3));
+
 
     // Check whether the Map contains a given value
+       System.out.println("Contains value 3? " + javaMap.containsValue(3));
 
     // Iterate over the keys of the Map, printing each key
+       System.out.println("\nKeys:");
+        for (String key : javaMap.keySet()) {
+            System.out.println(key);
+        }
 
     // Iterate over the values of the map, printing each value
+        System.out.println("\nValues:");
+        for (Integer value : javaMap.values()) {
+            System.out.println(value);
+        }
 
     // Iterate over the entries in the map, printing each key and value
+      System.out.println("\nEntries:");
+        for (Map.Entry<String, Integer> entry : javaMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
 
     /*
      * Usage tip!
--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,25 +1,30 @@
 public class NumberPractice {
   public static void main(String args[]) {
 
-    //
-   
 
 
     // Create a float with a negative value and assign it to a variable
+    float debt = -100;
 
 
     // Create an int with a positive value and assign it to a variable
+    int checking = 50;
   
-
     // Use the modulo % operator to find the remainder when the int is divided by 3
-
+    int remainder = checking % 3;
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
 
+    if( checking % 2 == 0){
+      System.out.println("Even");
+    }else System.out.println("odd");
+
     // Divide the number by another number using integer division
+  int result = checking / 4;
+System.out.println(result);
 
     /*
      * Reminder!
